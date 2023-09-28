4625415e1e7c7e72dbeaf754acbe1f694def3105
rajapedd <rajapeddanna.kadavakallu@gmail.com>
2023-09-29 00:25:03 +0530

    Spring Boot User API Added

--- src/main/java/com/raja/DemoApplication/controller/HomeController.java
+++ src/main/java/com/raja/DemoApplication/controller/HomeController.java
@@ -1,17 +1,24 @@
 package com.raja.DemoApplication.controller;
 
+import com.raja.DemoApplication.model.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-
-@Controller
-@ResponseBody
+@RestController
 public class HomeController {
 
 
     @RequestMapping("/")
     public String home(){
-        return "Hello Raja...!";
+        return "Hello Raja.............!";
+    }
+   // @RequestMapping(value = "/user" , method = RequestMethod.GET)
+    @GetMapping("/user")
+    public User setUsers() {
+        User u=new User();
+        u.setId("1");
+        u.setName("Raja");
+        u.setEmali("raja@gmail.com");
+        return u;
     }
 }
---
+++ src/main/java/com/raja/DemoApplication/model/User.java
@@ -0,0 +1,36 @@
+package com.raja.DemoApplication.model;
+
+
+
+public class User {
+
+    private String id;
+    private String name;
+    private String emali;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmali() {
+        return emali;
+    }
+
+    public void setEmali(String emali) {
+        this.emali = emali;
+    }
+
+
+}
